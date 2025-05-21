94aebcc3fa69365eeef7ac022f61d5e85c8b0d83
Sandr0ta <144434115+Sandr0ta@users.noreply.github.com>
2025-05-21 22:20:42 +0100

    Update Program.cs

--- HappySadNumberTask/Program.cs
+++ HappySadNumberTask/Program.cs
@@ -25,6 +25,47 @@
             //The program should take a number as input and output whether the number is happy or sad
             //test your program with the inputs of 19 and 20
             //show your output in the readme file
+
+            Console.Write("Enter a number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            
+            if (IsHappyNumber(number))
+            {
+                Console.WriteLine($"{number} is a happy number :D");
+            }
+            else
+            {
+                Console.WriteLine($"{number} is a sad number :(");
+            }
+        }
+
+        static bool IsHappyNumber(int num)
+        {
+            int slow = num;
+            int fast = GetNext(num);
+
+            // Floyd's cycle detection
+            while (fast != 1 && slow != fast)
+            {
+                slow = GetNext(slow);
+                fast = GetNext(GetNext(fast)); 
+            }
+
+            return fast == 1;
+        }
+
+        // Computes the sum of squares of digits
+        static int GetNext(int n)
+        {
+            int sum = 0;
+            while (n > 0)
+            {
+                int digit = n % 10;
+                sum += digit * digit;
+                n /= 10;
+            }
+            return sum;
+            
         }
     }
 }
